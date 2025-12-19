102039b730eaa108d3d40b91481ea69a1f7bebfc
Maksym Kapitaniuk <maxkapitaniuk@gmail.com>
2025-12-19 10:00:28 +0200

    Add hosting config to firebase.json

--- lib/core/providers/reading_provider.dart
+++ lib/core/providers/reading_provider.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart'; // XFile
+import 'package:image_picker/image_picker.dart';
 import '../../models/book.dart';
 import '../../repositories/books_repository.dart';
 import '../../repositories/auth_repository.dart';
